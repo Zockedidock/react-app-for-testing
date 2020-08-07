5fa3fc3df6161f884ff105b902303a0d655c0763
Joona Brückner <zockedidockhd@gmail.com>
2020-08-07 23:33:51 +0200

    Updates

---
+++ precache-manifest.6ffa6a984e27507e68b42f4af2355988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e648263e412b24e04556006e03232da",
+    "url": "/react-app-for-testing/index.html"
+  },
+  {
+    "revision": "15a938153984f4e43c03",
+    "url": "/react-app-for-testing/static/css/main.3e7f82ec.chunk.css"
+  },
+  {
+    "revision": "488cf16df927a877084f",
+    "url": "/react-app-for-testing/static/js/2.1d2f551f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-for-testing/static/js/2.1d2f551f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a938153984f4e43c03",
+    "url": "/react-app-for-testing/static/js/main.2fe97199.chunk.js"
+  },
+  {
+    "revision": "16d99fea6a0feb117956",
+    "url": "/react-app-for-testing/static/js/runtime-main.20d61e2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-for-testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-app-for-testing/precache-manifest.6ffa6a984e27507e68b42f4af2355988.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app-for-testing/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
