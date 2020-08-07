a4e7eb9aa97fde4a0ab6128081efab5a673e9953
Joona Brückner <zockedidockhd@gmail.com>
2020-08-08 00:12:35 +0200

    - fixed router issues it

--- src/Router/Home.js
+++ src/Router/Home.js
@@ -9,10 +9,10 @@ function Home() {
         <img src={logo} className="App-logo" alt="logo" />
         <div id="welcome">
           <h1 className="h3 mb-3 font-weight-normal">Welcome!</h1>
-          <a className="btn btn-lg btn-primary btn-block mt-4" href="/#/login">
+          <a className="btn btn-lg btn-primary btn-block mt-4" href="/react-app-for-testing/#/login">
             Login
           </a>
-          <a className="btn btn-lg btn-primary btn-block mt-4" href="/#/register">
+          <a className="btn btn-lg btn-primary btn-block mt-4" href="/react-app-for-testing/#/register">
             Register
           </a>
         </div>
